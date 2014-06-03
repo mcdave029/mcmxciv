1443dc6f1f301f30f4ff7660c5811907da101119
Mc Dave <mdvaldecantos@gmail.com>
2014-06-04 00:03:19 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Just as Simple app :)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mc::Application.config.secret_token = '7e434a6f08a0ce094b4d5535ee6624de1c7bb58ae9e6ce86cc130f1173376f5019ed8f9fea9f492ff6f976e9c25bf9c6cb967ea23bf7694fc3ca0050bc8d1289'
